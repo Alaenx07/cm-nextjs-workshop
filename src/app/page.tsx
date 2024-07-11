5f9256f0f4b0285d0ec3ee17f55b77601224bcf4
Alaenx07 <anouluck.phengvongsaa@gmail.com>
2024-07-11 16:40:33 +0700

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,40 @@ type Props = {};
 export default async function Home({}: Props) {
   const key = process.env.NEXT_PUBLIC_API_KEY;
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`;
-  const data = await fetch(url);
-  const res = await data.json();
-  // await delay(1000)
+
+  let res;
+
+  try {
+    const data = await fetch(url);
+    res = await data.json();
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+    res = { results: [] }; // Provide a default value in case of error
+  }
+
   return (
     <div>
-      <div className=" grid gap-3 grid-cols-fluid">
-        {res.results.map((movie: any) => (
-          <Movie
-            key={movie.id}
-            id={movie.id}
-            title={movie.title}
-            poster_path={movie.poster_path}
-            release_date={movie.release_date}
-          />
-        ))}
+      <div className="grid gap-3 grid-cols-fluid">
+        {res.results && res.results.length > 0 ? (
+          res.results.map((movie: any) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              poster_path={movie.poster_path}
+              release_date={movie.release_date}
+            />
+          ))
+        ) : (
+          <p>No movies available.</p>
+        )}
       </div>
     </div>
   );
 }
-// function delay(timeout: number){
-//   return new Promise((resolve)=>{
-//     setTimeout(resolve, timeout)
-//   })
+
+// function delay(timeout: number) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, timeout);
+//   });
 // }
